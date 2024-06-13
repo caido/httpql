e184c2a52aae95e9aa4d0e2992de45ef828e31a8
Emile Fugulin <code@efugulin.com>
2024-06-13 17:43:24 -0400

    Improve highlight tags

--- js/src/parser/props.ts
+++ js/src/parser/props.ts
@@ -2,16 +2,29 @@ import type { NodePropSource } from "@lezer/common";
 import { styleTags, tags } from "@lezer/highlight";
 
 export const highlight: NodePropSource = styleTags({
+  // Operators
   "IntExpression/IntOperator": tags.operator,
-  "IntExpression/IntValue": tags.integer,
   "StringExpression/StringOperator": tags.operator,
-  RequestIntFieldName: tags.attributeName,
+  "StringExpression/RegexOperator": tags.operator,
+  "DateExpression/DateOperator": tags.operator,
+
+  // Namespaces
   RequestNamespace: tags.attributeName,
-  RequestStringFieldName: tags.attributeName,
-  ResponseIntFieldName: tags.attributeName,
   ResponseNamespace: tags.attributeName,
   PresetNamespace: tags.attributeName,
-  SymbolValue: tags.literal,
+  RowNamespace: tags.attributeName,
+
+  // Fields
+  RequestIntFieldName: tags.attributeName,
+  RequestStringFieldName: tags.attributeName,
+  RequestDateFieldName: tags.attributeName,
+  ResponseIntFieldName: tags.attributeName,
   ResponseStringFieldName: tags.attributeName,
+  RowIntFieldName: tags.attributeName,
+
+  // Values
+  SymbolValue: tags.literal,
+  "IntExpression/IntValue": tags.integer,
   "StringValue/...": tags.string,
+  "RegexValue/...": tags.string,
 });
